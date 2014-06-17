8239ee67c3a87b2069a847f27d2444bb75f1ccf6
Kyle Zentner <zentner.kyle@gmail.com>
2014-06-16 21:40:32 -0700

    Fixed nikita's code.

--- angel-player/src/chrome/content/common/lua_interface.js
+++ angel-player/src/chrome/content/common/lua_interface.js
@@ -3,7 +3,7 @@ var lua = require('lua');
 function lua_add_function(lua_state, name, js_fn) {
   lua.ccall('lua_pushlightuserdata',
             'number', ['number', 'number'],
-            [lua_state, js_fn]);
+            [lua_state, lua.Runtime.addFunction(js_fn)]);
   lua.ccall('lua_setglobal',
             'number', ['number', 'string'],
             [lua_state, name]);
@@ -18,12 +18,12 @@ function make() {
   lua.ccall('registerJS', 'number', ['number'], [lua_state]);
 
   var res = {
-    lua_state : lua.ccall('luaL_newstate', 'number'),
+    lua_state : lua_state,
     add_fn : function(name, js_fn) {
       lua_add_function(res.lua_state, name, js_fn);
     },
     load_text : function(text) {
-      var retcode = lua.ccall('luaL_loadstring', 'number', ['number', 'string'], [lua_state, text]);
+      var retcode = lua.ccall('luaL_loadstring', 'number', ['number', 'string'], [res.lua_state, text]);
       if (retcode !== 0) {
         throw "Invalid code!";
       }
