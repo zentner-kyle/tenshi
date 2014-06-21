1d3439d2733ab61104457e1b874e14764e9150c0
Kyle Zentner <zentner.kyle@gmail.com>
2014-06-21 15:57:54 -0700

    Updated test.js to use thread functionality.

--- vm/lua/test.js
+++ vm/lua/test.js
@@ -2,9 +2,12 @@ var lua = require('./build/lua/release_emscripten/lua.js');
 var log = lua.Runtime.addFunction(console.log);
 
 var code = '\n' +
-'fastCallJSFunction(log, "test!")\n' +
-'fastCallJSFunction(log, 1, 2, 3.1)\n' +
-'\n';
+  'i = 1\n' +
+  'while true do\n' +
+  '  i = i + 1\n' +
+  '  fastCallJSFunction(log, i)\n' +
+  'end\n' +
+  '\n';
 
 var lua_state = lua.ccall('luaL_newstate', 'number');
 lua.ccall('registerJS', 'number', ['number'], [lua_state]);
@@ -16,6 +19,5 @@ var res = lua.ccall('luaL_loadstring', 'number', ['number', 'string'], [lua_stat
 if (res !== 0) {
   console.log('load failed!');
 }
-lua.ccall('lua_pcallk', 'number',
-          ['number', 'number', 'number', 'number', 'number', 'number'],
-          [lua_state, 0, 0, 0, 0, 0]);
+lua.ccall('thread_setup', 'string', ['number'], [lua_state]);
+console.log(lua.ccall('thread_run_ticks', 'string', ['number', 'number'], [lua_state, 1000]));
