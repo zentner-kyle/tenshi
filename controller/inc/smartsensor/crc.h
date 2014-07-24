58d97fb1a2a610270b5b8f285e8bba60fe7dc7c4
Casey Duckering <cduckering@gmail.com>
2014-07-24 02:25:58 -0700

    Switch CRC8 algorithm and use table lookup

--- controller/inc/smartsensor/crc.h
+++ controller/inc/smartsensor/crc.h
@@ -2,9 +2,8 @@
 #define INC_SMARTSENSOR_CRC_H_
 
 #include <stdint.h>
-// For size_t
 #include <string.h>
 
-uint8_t crc8(uint8_t crc, uint8_t *data, size_t len);
+uint8_t crc8(uint8_t crc, const uint8_t *data, size_t len);
 
 #endif  // INC_SMARTSENSOR_CRC_H_
