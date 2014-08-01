be6eb4fc2885aa82b7e3e0a16ef741c3708280ec
Kyle Zentner <zentner.kyle@gmail.com>
2014-08-01 00:24:53 -0700

    testing!!!!

--- angel-player/src/chrome/content/welcome/welcome.js
+++ angel-player/src/chrome/content/welcome/welcome.js
@@ -28,6 +28,7 @@ function connectRadio() {
     global_state.set('serial_port_object', serportObj);
 
     let addr = ioports.get_xbee_addr();
+      //global_state.get('robot_application').radio_pairing_info;
     let main_radio = global_state.get('main_radio');
     main_radio.connectXBee(addr, serportObj);
 
