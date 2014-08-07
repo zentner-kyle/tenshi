293746054404f7ed5a44e9e19449b5c49f77ad66
Kyle Zentner <zentner.kyle@gmail.com>
2014-08-06 22:44:46 -0700

    Revert "Remove debug statements from ndl3.c"
    This reverts commit 34d547ca0a90cc9f3128f6da63be1c66da0402a9.
    
    Conflicts:
    	network/src/ndl3.c

--- network/src/ndl3.c
+++ network/src/ndl3.c
@@ -540,6 +540,7 @@ void NDL3_L2_pop(NDL3Net * restrict net,
 
 static void NDL3_L2_push_in_pkt(NDL3Net * restrict net, semi_packet * pkt,
                          L2_data_packet * L2pkt) {
+  printf("recv side got ");
   /*
    * Called for packets that are being received. Does not handle START packets,
    * because those are somewhat more complicated and thus have their own
@@ -547,9 +548,11 @@ static void NDL3_L2_push_in_pkt(NDL3Net * restrict net, semi_packet * pkt,
    * semi_packet.
    */
   if (L2pkt->type == END_PACKET) {
+    printf("end\n");
     pkt->state |= PACKET_CLOSING;
     net->free(pkt->data, net->userdata);
   } else if (L2pkt->type == DATA_PACKET) {
+    printf("data\n");
     if (L2pkt->offset != pkt->last_offset) {
       /*
        * We received an out of order data packet, so set the PACKET_BACK flag.
@@ -570,15 +573,19 @@ static void NDL3_L2_push_in_pkt(NDL3Net * restrict net, semi_packet * pkt,
 
 static void NDL3_L2_push_out_pkt(NDL3Net * restrict net, semi_packet * pkt,
                          L2_data_packet * L2pkt) {
+  printf("send side got ");
   /* Called for packets that are being sent. */
   if (L2pkt->type == ACK_PACKET) {
+    printf("ack\n");
     pkt->last_acked_offset = L2pkt->offset;
     pkt->time_last_ack = net->time;
   } else if (L2pkt->type == BACK_PACKET) {
+    printf("back\n");
     pkt->last_offset = L2pkt->offset;
     pkt->last_acked_offset = L2pkt->offset;
     pkt->time_last_ack = net->time;
   } else if (L2pkt->type == FIN_PACKET) {
+    printf("fin\n");
     pkt->state |= PACKET_CLOSING;
     net->free(pkt->data, net->userdata);
   } else {
@@ -592,6 +599,7 @@ static void push_start(NDL3Net * restrict net, NDL3_port port,
    * Handle receiving START packet. Only place where memory is currently
    * allocated by this system.
    */
+  printf("recv side got start\n");
   for (int j = 0; j < NDL3_PACKETS_PER_PORT; j++) {
     semi_packet * pkt = &net->ports[port].in_pkts[j];
     if (pkt->state == PACKET_EMPTY) {
