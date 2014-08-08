f9e5ba588ebe59ea29cc25c56ab609dbe8433c32
Kyle Zentner <zentner.kyle@gmail.com>
2014-08-07 23:50:57 -0700

    Add debug statement to recv_L2.

--- angel-player/src/chrome/content/common/radio.js
+++ angel-player/src/chrome/content/common/radio.js
@@ -138,6 +138,7 @@ function recv_L2 (rxbuf) {
   /* jshint validthis: true */
   // We need to extract the payload from the xbee packet.
   var data = xbee.extractPayload(rxbuf);
+  console.log('payload', data);
   var ptr = emcc_tools.buffer_to_ptr(ndl3, data);
   call('NDL3_L2_push', this.net, ptr, data.length);
   call('free', ptr);
